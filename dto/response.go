8477636ff84c5b6b45b4624250442a854d5ff970
Maulana Ishak <98244927+MaulIshak@users.noreply.github.com>
2025-05-10 19:00:52 +0700

    fix: correct JSON tags in Response struct

--- dto/response.go
+++ dto/response.go
@@ -1,9 +1,9 @@
 package dto
 
 type Response[T any] struct {
-	Code    string `json:code`
-	Message string `json:code`
-	Data    T      `json:data`
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Data    T      `json:"data"`
 }
 
 func CreateResponseError(message string) Response[string] {
