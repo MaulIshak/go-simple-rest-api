938ade7346ad505bd3b17fd034042a8d5c4910d7
Maulana Ishak <98244927+MaulIshak@users.noreply.github.com>
2025-05-11 11:25:37 +0700

    feat: add update customer functionality with request validation

--- internal/service/customer.go
+++ internal/service/customer.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"go-simple-rest-api/domain"
 	"go-simple-rest-api/dto"
 	"time"
@@ -13,6 +14,7 @@ import (
 type customerService struct {
 	customerRepository domain.CustomerRepository
 }
+
 
 // Create implements domain.CustomerService.
 
@@ -30,7 +32,7 @@ func (c customerService) Index(ctx context.Context) ([]dto.CustomerData, error) 
 	}
 
 	var customersData []dto.CustomerData
-
+	
 	for _, val := range customers {
 		customersData = append(customersData, dto.CustomerData{
 			ID:   val.ID,
@@ -43,11 +45,27 @@ func (c customerService) Index(ctx context.Context) ([]dto.CustomerData, error) 
 
 func (c *customerService) Create(ctx context.Context, req dto.CreateCustomerRequest) error {
 	customer := domain.Customer{
-		ID: uuid.NewString(),
-		Code: req.Code,
-		Name: req.Name,
+		ID:        uuid.NewString(),
+		Code:      req.Code,
+		Name:      req.Name,
 		CreatedAt: sql.NullTime{Valid: true, Time: time.Now()},
 	}
-
+	
 	return c.customerRepository.Save(ctx, &customer)
+}
+// Update implements domain.CustomerService.
+func (c *customerService) Update(ctx context.Context, req dto.UpdateCustomerRequest) error {
+	persisted, err := c.customerRepository.FindById(ctx, req.ID)
+	if err != nil{
+		return err
+	}
+	if persisted.ID == ""{
+		return errors.New("data customer tidak ditemukan")
+
+	}
+	persisted.Code = req.Code
+	persisted.Name = req.Name
+	persisted.UpdatedAt = sql.NullTime{Valid:true, Time: time.Now()}
+
+	return c.customerRepository.Update(ctx, &persisted)
 }
